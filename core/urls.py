651f4cf73fd42d14b4d78da1a915cef254cb5a70
Yuri Boeira <yuriboeira11tx@gmail.com>
2024-05-28 19:08:21 -0300

    versao atual

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.map_view, name='map_view'),
     path('ocorrencia/<int:pk>/', views.ocorrencia_detail_view, name='ocorrencia_detail'),
+    path('register-occurrence/', views.register_occurrence, name='register_occurrence'),
+    path('cadastro/', views.cadastro_view, name='cadastro'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.clogout, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.core.serializers import serialize
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.models import User
 from .models import Ocorrencia
 
-# def map_view(request):
-#     ocorrencias = Ocorrencia.objects.all()
-#     return render(request, 'core/map.html', {'ocorrencias': ocorrencias})
 
 def map_view(request):
     ocorrencias = Ocorrencia.objects.all()
@@ -14,3 +13,46 @@ def map_view(request):
 def ocorrencia_detail_view(request, pk):
     ocorrencia = get_object_or_404(Ocorrencia, pk=pk)
     return render(request, 'core/ocorrencia_detail.html', {'ocorrencia': ocorrencia})
+
+def register_occurrence(request):
+    if request.method == 'POST':
+        form = OcorrenciaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('map_view')
+    else:
+        form = OcorrenciaForm()
+    return render(request, 'register_occurrence.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('map_view')
+        else:
+            pass
+
+    return render(request, 'core/login.html')
+
+def clogout(request):
+    logout(request)
+    return redirect('map_view')
+
+def cadastro_view(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        if User.objects.filter(username=username).exists():
+            pass
+        else:
+            user = User.objects.create_user(username=username, password=senha)
+            user.first_name = nome
+            user.save()
+            return redirect('login')
+    
+    return render(request, 'core/cadastro.html')
